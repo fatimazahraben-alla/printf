e7d4768a45993d8a638706d260353ead5e1d0131
fatimazahraben-alla <fatisafae33@gmail.com>
2023-04-24 12:42:47 -0700

    add 0-printf.c

---
+++ 0-printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ *_printf - printf func
+ *@format: first arg
+ *Return: int
+ */
+int _printf(const char *format, ...)
+{
+	int length = 0;
+	char *s, c;
+	va_list ars;
+
+	va_start(ars, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					c = va_arg(ars, int);
+					_putchar(c);
+					break;
+				case 's':
+					s = va_arg(ars, char *);
+					while (*s != '\0')
+					{
+					_putchar(*s);
+					s++
+					}
+					break;
+				case '%':
+					_putchar('%');
+					break;
+			}
+		}
+		else
+			_putchar(*format);
+			length++;
+		format++;
+	}
+	va_end(ars);
+	return (length);
+}
